6ed0e78365cbda0aa4af105186dce31a075a6b30
Micheal Burin <Micheal.Burin@colorado.edu>
2016-09-16 13:26:15 -0600

    Fixed my_itoa to work with negative numbers.

--- suite_my_itoa.c
+++ suite_my_itoa.c
@@ -32,8 +32,62 @@ char test_itoa_max_base16(){
 
     my_itoa(result,2147483647,16);
     printf("result is: %s\n",result);
-    ASSERT(strcmp(result,"1fffffff") == 0);
+    ASSERT(strcmp(result,"7fffffff") == 0);
     if (mylib_errno != MYLIB_ERR_NULL) {free (result);}
 }
 
+char test_itoa_max_0_base16(){
+    SETUP;
+    
+    int8_t *result= calloc(MYLIB_MAX_DIGITS,1);
 
+    my_itoa(result,0,16);
+    printf("result is: %s\n",result);
+    ASSERT(strcmp(result,"0") == 0);
+    if (mylib_errno != MYLIB_ERR_NULL) {free (result);}
+}
+
+//Negative number tests
+char test_itoa_12345_neg(){
+    SETUP;
+    
+    int8_t *result= calloc(MYLIB_MAX_DIGITS,1);
+
+    my_itoa(result,-12345,10);
+    printf("result is: %s\n",result);
+    ASSERT(strcmp(result,"-12345") == 0);
+    if (mylib_errno != MYLIB_ERR_NULL) {free (result);}
+}
+
+char test_itoa_max_binary_neg(){
+    SETUP;
+    
+    int8_t *result= calloc(MYLIB_MAX_DIGITS,1);
+
+    my_itoa(result,-2147483648,2);
+    printf("result is: %s\n",result);
+    ASSERT(strcmp(result,"-10000000000000000000000000000000") == 0);
+    if (mylib_errno != MYLIB_ERR_NULL) {free (result);}
+}
+
+char test_itoa_max_base10_neg(){
+    SETUP;
+    
+    int8_t *result= calloc(MYLIB_MAX_DIGITS,1);
+
+    my_itoa(result,-2147483648,10);
+    printf("result is: %s\n",result);
+    ASSERT(strcmp(result,"-2147483648") == 0);
+    if (mylib_errno != MYLIB_ERR_NULL) {free (result);}
+}
+
+char test_itoa_max_base16_neg(){
+    SETUP;
+    
+    int8_t *result= calloc(MYLIB_MAX_DIGITS,1);
+
+    my_itoa(result,-2147483648,16);
+    printf("result is: %s\n",result);
+    ASSERT(strcmp(result,"-80000000") == 0);
+    if (mylib_errno != MYLIB_ERR_NULL) {free (result);}
+}
