d5578f9736d60a0dda9c592354ede096090d11df
Micheal Burin <Micheal.Burin@colorado.edu>
2016-09-15 23:55:50 -0600

    Added some itoa unit tests.

--- Makefile
+++ Makefile
@@ -22,4 +22,4 @@ test_suites.o: $(SOURCES)
 	$(CC) $(CFLAGS) $(INCLUDES) -c $^ 
 
 clean:
-	-rm -f test_suites.o test_functions.c $(OBJS) run_tests core
+	-rm -f driver.o test_suites.o test_functions.c test_functions.o $(OBJS) run_tests core
--- suite_my_itoa.c
+++ suite_my_itoa.c
@@ -1,14 +1,38 @@
+#include <stdlib.h>
 #include "test_suite.h"
 #include "data.h"
 
-char test_itoa_null1(){
+char test_itoa_max_binary(){
     SETUP;
     
-    int8_t *result= malloc(MYLIB_MAX_DIGITS);
+    int8_t *result= calloc(MYLIB_MAX_DIGITS,1);
 
     my_itoa(result,2147483647,2);
     printf("result is: %s\n",result);
-    ASSERT(mylib_errno == MYLIB_ERR_OK);
+    ASSERT(strcmp(result,"1111111111111111111111111111111") == 0);
     if (mylib_errno != MYLIB_ERR_NULL) {free (result);}
 }
+
+char test_itoa_max_base10(){
+    SETUP;
+    
+    int8_t *result= calloc(MYLIB_MAX_DIGITS,1);
+
+    my_itoa(result,2147483647,10);
+    printf("result is: %s\n",result);
+    ASSERT(strcmp(result,"2147483647") == 0);
+    if (mylib_errno != MYLIB_ERR_NULL) {free (result);}
+}
+
+char test_itoa_max_base16(){
+    SETUP;
+    
+    int8_t *result= calloc(MYLIB_MAX_DIGITS,1);
+
+    my_itoa(result,2147483647,16);
+    printf("result is: %s\n",result);
+    ASSERT(strcmp(result,"1ffffff") == 0);
+    if (mylib_errno != MYLIB_ERR_NULL) {free (result);}
+}
+
 
