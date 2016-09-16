927c14dc55bc4c8451e306965c5540e758e6b924
Micheal Burin <Micheal.Burin@colorado.edu>
2016-09-16 11:31:38 -0600

    Added dump_memory, finished endianness, and added appropriate unit tests

---
+++ suite_my_dump_memory.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <string.h>
+#include "test_suite.h"
+#include "data.h"
+
+char test_my_dump_memory(){
+    SETUP;
+    
+    uint8_t memory[] = "This is memory to be printed out nicely. 0987654321" \
+                       "abcdefghijklmnopqrstuvwxyz !@#$%^&*~";
+    //print out string +NULL
+    dump_memory(&memory[0],strlen(&memory[0]) + 1);
+    ASSERT(mylib_errno == MYLIB_ERR_OK);
+}
+
+char test_my_dump_random_memory(){
+    SETUP;
+    
+    uint8_t *memory=malloc(1000);
+    dump_memory(memory,1000);
+    ASSERT(mylib_errno == MYLIB_ERR_OK);
+    free(memory);
+}
+
+char test_my_dump_memory_random(){
+    SETUP;
+    
+    uint8_t *memory=malloc(256);
+    for(int i=0;i<256;i++) {
+        memory[i] = random();
+    }
+    dump_memory(memory,256);
+    ASSERT(mylib_errno == MYLIB_ERR_OK);
+    free(memory);
+}
---
+++ suite_my_endian.c
@@ -0,0 +1,37 @@
+#include <string.h>
+#include "test_suite.h"
+#include "data.h"
+
+char test_my_big_to_little_1(){
+    SETUP;
+    
+    uint32_t result=big_to_little(0xabcdef12); 
+    printf("result is: %x\n",result);
+    ASSERT(result == 0x12efcdab);
+}
+
+char test_my_big_to_little_2(){
+    SETUP;
+    
+    uint32_t result=big_to_little(0x0); 
+    printf("result is: %x\n",result);
+    ASSERT(result == 0x0);
+}
+
+char test_my_little_to_big_1(){
+    SETUP;
+    
+    uint32_t result=big_to_little(0xabcdef12); 
+    printf("result is: %x\n",result);
+    ASSERT(result == 0x12efcdab);
+}
+
+char test_my_little_to_big_2(){
+    SETUP;
+    
+    uint32_t result=big_to_little(0x0); 
+    printf("result is: %x\n",result);
+    ASSERT(result == 0x0);
+}
+
+
