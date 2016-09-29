bdd95e67992f5e0ceacad555dc6cbafa32604998
burin <Micheal.Burin@colorado.edu>
2016-09-29 15:23:24 -0600

    Starting Project2

---
+++ circbuf.c
@@ -0,0 +1,82 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "circbuf.h"
+
+circbuf_t *circbuf_init(uint32_t size) {
+circbuf_t *c;
+
+    c=malloc(sizeof(circbuf_t));
+    c->buf=calloc(size,0);
+    c->size=0;
+    c->last_index=size-1;
+    c->head=0;
+    c->tail=0;
+    c->state=CBUF_OK;
+
+return c;
+}
+
+void circbuf_destroy(circbuf_t * c) {
+    if (c->buf) { free(c->buf); }
+    if (c) { free(c); }
+}
+
+void circbuf_print(circbuf_t * c) {
+    if (!c) { return; }
+    for (int i=0;i<c->last_index +1;i++) {
+        printf("%.03d ",*((c->buf)+i));
+    }
+    printf("\n");
+    for (int i=0;i<c->head;i++) {
+        printf("    ");
+    }
+    printf("^__head\n");
+
+    for (int i=0;i<c->tail;i++) {
+        printf("    ");
+    }
+    printf("^__tail\n");
+}
+
+uint8_t circbuf_push(circbuf_t * c, uint8_t data) {
+    if (!c) { return CBUF_ERROR; }
+    if (c->state != CBUF_OK) { return c->state; }
+    c->state=CBUF_OK;
+
+    *((c->buf) + c->head) = data;
+    if (c->head == c->last_index) {
+        c->head = 0;
+    } else {
+        c->head++;
+    }
+
+    c->size++;
+    /* Set internal flag if buffer is full */
+    if (c->size == c->last_index + 1) { c->state = CBUF_FULL; } /* full */
+
+return c->state;
+}
+
+uint8_t circbuf_pop(circbuf_t * c, uint8_t *data) {
+    if (!c) { return CBUF_ERROR; }
+    if ( c->state != CBUF_OK ) { return c->state; }
+    c->state=CBUF_OK;
+
+    uint8_t *tail_ptr=((c->buf) + c->tail);
+    *data = *tail_ptr;
+    *tail_ptr =255;
+
+    if (c->tail == c->last_index) {
+        c->tail = 0;
+    } else {
+        c->tail++;
+    }
+
+    c->size--;
+
+    /* Empty */
+    if (c->size == 0) { c->state=CBUF_EMPTY; } /* empty */
+
+return c->state;
+}
+
---
+++ circbuf.h
@@ -0,0 +1,89 @@
+#ifndef __CIRCBUF_H__
+#define __CIRCBUF_H__
+
+#include <stdint.h>
+
+/* Circular Buffer implementation (one piece of memory)
+ * Holds up to 2^32 8bit numbers - FIFO circular stack
+ *
+ * In this implementation, tail points to the current
+ * member to pop off the stack. Head points to the next
+ * empty slot. head == tail both on empty and full.
+ * So we use "size" to keep track of the current size
+ * last_index is an indirect way to keep track of allocated size 
+ */
+
+/* enum not used, but this is one idea */
+typedef enum { CBUF_NOTINIT=0, CBUF_OK, CBUF_ERROR, CBUF_EMPTY, CBUF_FULL } buf_state_e;
+
+typedef struct {
+        uint8_t *buf;
+        uint32_t size;
+        uint32_t last_index;
+        uint32_t head;
+        uint32_t tail;
+        buf_state_e state; //Not implemented
+} circbuf_t;
+
+/*
+ * Initialize circular buffer.
+ * This routine take a size in bytes and allocates
+ * buffer and sets all items to zero.
+ *
+ * Input - size in bytes
+ * return - pointer to a circbuf_t
+ *        - NULL if memory not allocated
+ *
+ */
+circbuf_t *circbuf_init(uint32_t size);
+
+/*
+ * Destroy circular buffer
+ * This routine deletes resources and 
+ * memory.
+ *
+ * Input - pointer to a circbuf_t
+ * Return - none
+ */
+void circbuf_destroy(circbuf_t * c);
+
+/*
+ * Add item to stack (push)
+ *
+ * Input - pointer to circbuf_t 
+ *  data - uint8_t
+ *
+ * return - CIRCBUF_ERROR on overflow
+ *        - CIRCBUF_OK, item added sucessfully
+ *        - CIRCBUF_NULL, null buffer passed in 
+ */
+uint8_t circbuf_push(circbuf_t * c, uint8_t data);
+
+/*
+ * Remove item from stack (pop)
+ *
+ * Input - pointer to circbuf_t 
+ *  *pointer* to uint8_t
+ * Output
+ *  data put in *pointer*
+ *
+ * return - CIRCBUF_ERROR on empty
+ *        - CIRCBUF_OK, item removed sucessfully
+ *        - CIRCBUF_NULL, null buffer passed in 
+ */
+uint8_t circbuf_pop(circbuf_t * c, uint8_t *data);
+
+/* 
+ * Print out buffer (small sizes only) for debug
+ *
+ *
+ * Input - pointer to circbuf_t 
+ *
+ * return - CIRCBUF_NULL, null buffer passed in 
+ *
+ */
+void circbuf_print(circbuf_t * c);
+
+
+
+#endif
