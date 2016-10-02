01060aa2db448ab379af71c735375af1ae0d74e5
Micheal Burin <Micheal.Burin@colorado.edu>
2016-10-02 15:57:53 -0600

    Progress on floating point conversions

--- suite_my_circbuf.c
+++ suite_my_circbuf.c
@@ -71,7 +71,6 @@ RETURN;
 char test_my_circbuf_push_pop2(){
     SETUP;
 
-    uint8_t expected[]={ 1,2,3,4,5,6,7,8,9,10 };
     uint8_t out;
     circbuf_t *c;
     c=circbuf_init(10);
@@ -133,6 +132,93 @@ char test_my_circbuf_pop_overflow(){
 RETURN;
 }
 
+char test_my_circbuf_push_pop3(){
+    SETUP;
+
+    uint8_t expected1[]={ 255,255,255,255,255,255,70,71,72,255 };
+    uint8_t expected2[]={ 255,255,255,255,255,255,255,255,255,255 };
+    
+    uint8_t out=0;
+    uint8_t *buf;
+    circbuf_t *c;
+    c=circbuf_init(10);
+
+for (int j=1;j<20;j++) {
+        if ( circbuf_is_pushable(c) ) {
+        circbuf_push(c,j);
+        printf ("push a %d\n",j);
+        }
+}
+
+if ( circbuf_is_poppable(c) ) {
+    circbuf_pop(c,&out);
+    printf("Popped a \"%d\"\n",out);
+}
+
+if ( circbuf_is_pushable(c) ) {
+    circbuf_push(c,88);
+    printf("Pushed a \"%d\"\n",88);
+}
+
+
+for (int i=0;i<20;i++) {
+    if ( circbuf_is_poppable(c) ) {
+        circbuf_pop(c,&out);
+        printf("Popped a \"%d\"\n",out);
+    }
+}
+
+for (int j=50;j<55;j++) {
+        if ( circbuf_is_pushable(c) ) {
+        circbuf_push(c,j);
+        printf ("push a %d\n",j);
+        }
+}
+
+for (int i=0;i<4;i++) {
+    if ( circbuf_is_poppable(c) ) {
+        circbuf_pop(c,&out);
+        printf("Popped a \"%d\"\n",out);
+    }
+}
+
+for (int i=0;i<20;i++) {
+    if ( circbuf_is_poppable(c) ) {
+        circbuf_pop(c,&out);
+        printf("Popped a \"%d\"\n",out);
+    }
+}
+
+circbuf_print(c);
+for (int j=70;j<73;j++) {
+        if ( circbuf_is_pushable(c) ) {
+        circbuf_push(c,j);
+        printf ("push a %d\n",j);
+        }
+}
+circbuf_print(c);
+
+
+    test_print_array("Expected",&expected1[0],10);
+    buf=__circbuf_dump_core(c);
+    test_print_array("Result  ",buf,10);
+    ASSERT (strncmp((const char*)buf,(const char*)&expected1[0],10) == 0);
+
+for (int i=0;i<20;i++) {
+    if ( circbuf_is_poppable(c) ) {
+        circbuf_pop(c,&out);
+        printf("Popped a \"%d\"\n",out);
+    }
+}
+
+    test_print_array("Expected",&expected2[0],10);
+    buf=__circbuf_dump_core(c);
+    test_print_array("Result  ",buf,10);
+    ASSERT (strncmp((const char*)buf,(const char*)&expected2[0],10) == 0);
+        
+    if (c) { circbuf_destroy(c); }
+RETURN;
+}
 
 
 
