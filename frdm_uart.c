d773bff4f0a37a1c7a518c6e1efd38b51e7ceaef
burin <Micheal.Burin@Colorado.edu>
2016-10-29 01:19:21 -0600

    Hexadecimal fix

--- frdm_uart.c
+++ frdm_uart.c
@@ -21,7 +21,7 @@ void write_uart0_va(const char* fmt, ...) {
 void uart0_write_x(uint32_t n) {
 
     int8_t *result= (int8_t*)calloc(MYLIB_MAX_DIGITS,1);
-    my_itoa(result,n,16);
+    my_utoa(result,n,16);
 
     for (int i=0;i<MYLIB_MAX_DIGITS;i++) {
         if (i > 127 ) { i= 32;}
