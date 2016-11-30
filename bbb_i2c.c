26e4afe38df4de6b9b979e68f6c99492c57c3213
burin <Micheal.Burin@Colorado.edu>
2016-11-30 10:37:55 -0700

    Changes for i2c bit banged on BBB

--- bbb_i2c.c
+++ bbb_i2c.c
@@ -5,29 +5,21 @@
 #include <unistd.h>
 
 static int i2c_clk;
-static int i2c_data;
-static int i2c_data_dir;
+static int i2c_data_out;
+static int i2c_data_in;
 
 void i2c_open_device() {
     i2c_clk=open("/sys/class/gpio/gpio5/value",O_WRONLY);
-    i2c_data=open("/sys/class/gpio/gpio4/value",O_RDWR);
-    i2c_data_dir=open("/sys/class/gpio/gpio4/direction",O_WRONLY);
+    i2c_data_out=open("/sys/class/gpio/gpio4/value",O_WRONLY);
+    i2c_data_in=open("/sys/class/gpio/gpio3/value",O_RDONLY);
 }
 
 void i2c_data_low() {
-    write(i2c_data,"0",1);
+    write(i2c_data_out,"0",1);
 }
 
 void i2c_data_high() {
-    write(i2c_data,"1",1);
-}
-
-void i2c_data_dir_in() {
-    write(i2c_data_dir,"in",2);
-}
-
-void i2c_data_dir_out() {
-    write(i2c_data_dir,"out",3);
+    write(i2c_data_out,"1",1);
 }
 
 void i2c_clock_low() {
@@ -48,10 +40,8 @@ return (b == '1' ? 1 : 0);
 
 i2c_level_e i2c_data_read() {
 uint8_t b=0;
-//i2c_data_dir_in();
-    lseek(i2c_data,0,SEEK_SET);
-    read(i2c_data,&b,1);
-//i2c_data_dir_out();
+    lseek(i2c_data_in,0,SEEK_SET);
+    read(i2c_data_in,&b,1);
 return (b == '1' ? 1 : 0);
 
 }
