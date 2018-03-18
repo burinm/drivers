a8bbb6f733dc2c7d4db8c90808572732f1c129b7
burin <21989631+burinm@users.noreply.github.com>
2018-03-18 14:25:06 -0600

    Drivers put into subdirectories

--- myerrno.c
+++
@@ -1,4 +0,0 @@
-/* myerrno.c - (c) 2016 - burin */
-
-#include <stdint.h>
-uint8_t mylib_errno=0;
--- printf.c
+++
@@ -1,8 +0,0 @@
-/* printf.c - (c) 2016 - burin */
-#include "printf.h"
-
-#ifdef SILENCE_PRINTF
-int myprintf(const char *format, ...) {
-return 0;
-}
-#endif
